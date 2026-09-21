2a1c01b923ef8fa3d5b63e4c9997c12d9f5187a0
Bella Mendoza <imendoza@doc.gov>
2026-09-21 11:02:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -933,24 +933,16 @@ title = st.text_input(
 
 
 description = st.text_area(
-    "Dataset description in plain language. **(Required)**",
-    height=160,
-    key=f"description_{dataset_number}"
-)
-st.markdown(
-    """
-    <div class="question-help">
-    Example:<br>
+    "Dataset description in plain language. **(Required)**"
+    "Example: "
     “The Current Population Survey (CPS) is a monthly survey of households conducted
     by the Census Bureau for the Bureau of Labor Statistics. In addition to the
     national unemployment rate, it provides data on employment, the unemployment
     rate, persons not in the labor force, hours of work, earnings, and other
-    demographic and labor force characteristics.”
-    </div>
-    """,
-    unsafe_allow_html=True
+    demographic and labor force characteristics.”,
+    height=160,
+    key=f"description_{dataset_number}"
 )
-
 
 # ============================================================
 # Q5 IDENTIFIER
